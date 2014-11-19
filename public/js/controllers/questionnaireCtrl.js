b0b9e2ba3a385d14b8c0ac263719ed84552609ae
yln <sourgrapes612@hotmail.com>
2014-11-19 01:30:22 -0600

    qa interface

--- public/js/controllers/questionnaireCtrl.js
+++ public/js/controllers/questionnaireCtrl.js
@@ -1,31 +1,42 @@
 angular.module("plantApp")
 .controller("questionnaireCtrl",function($scope, QA){
 	$scope.showquestion = false;
-	$scope.type = 0;
-	$scope.getquestion = function(){
-		typeOfQ = $scope.type
+	var submitbtn = angular.element("#submitbtn");
+	var msg=angular.element("#reply");
+	var msgicon=angular.element("#replyicon");
+	var selecticon=angular.element("#selecticon");
+	angular.element('.ui.dropdown').dropdown();
+
+	$scope.getquestion = function(typeOfQ){
+		submitbtn.removeClass("disabled");
+		selecticon.removeClass("dropdown icon");
+		selecticon.addClass("loading icon");
+		msg.attr("class","");
+	  msgicon.attr("class","");
 		$scope.showanswer = false;
-		$scope.showquestion = false;
-		$scope.showbutton = false;
-		$scope.showbar = true;
 		$scope.problem = null;
 		$scope.solution = null;
 		$scope.reply = null;
 		QA.get({id : typeOfQ}).$promise.then(function(data){
-			$scope.problem = data
+			$scope.problem = data;
+			$scope.qtitle = data.qtitle;
 			$scope.question = data.question;
 			$scope.question2 = data.equation2;
 			$scope.answer = data.answer;
-			$scope.showbutton = true;
-			$scope.showquestion = true;
-			$scope.showbar = false;
+			selecticon.removeClass("loading icon");
+			selecticon.addClass("dropdown icon");
+			$scope.placeholder="Please enter your answer here"
+			if(typeOfQ==1)
+				$scope.placeholder="Please enter your answer in the format of (x,y)";
+			var form = angular.element("#qaform");
+			form.modal('show');
 		});		
 	}
 	$scope.getanswer = function(){
 		if($scope.problem!=null){
 			$scope.answer = $scope.problem.answer;
 			$scope.showanswer = true;
-			$scope.showbutton = false;
+			submitbtn.addClass("disabled");
 		}
 	}
 
@@ -35,10 +46,21 @@ angular.module("plantApp")
 			var answer = $scope.problem.answer;
 			solution =solution.toString().replace(/ |\s+/g,"");
 			answer =answer.toString().replace(/ |\s+/g,"");
-			if(solution == answer)
-				$scope.reply="correct";
-			else
-				$scope.reply="wrong";
+			if(solution == answer){
+				$scope.reply="Correct";
+				msg.removeClass("ui icon error message");
+				msg.addClass("ui icon success message");
+				msgicon.removeClass("frown icon");
+				msgicon.addClass("smile icon");
+				submitbtn.addClass("disabled");
+			}
+			else{
+				$scope.reply="Incorrect";
+				msg.removeClass("ui icon success message");
+				msg.addClass("ui icon error message");
+				msgicon.removeClass("smile icon");
+				msgicon.addClass("frown icon");
+			}
 		}
 	}
 	
